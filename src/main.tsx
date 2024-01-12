639b5619950d9a735bb5689882187bf4899eef7c
Eugene <91225197+jewhyena@users.noreply.github.com>
2024-01-12 14:04:47 +0200

    Remove space

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <div className="bg-[#101010] flex flex-col h-screen w-full justify-center items-center">
       <div className="bg-[#1b1b1b] max-w-[600px] w-full pl-6 pr-2.5 pt-6 rounded-2xl">
         <p className="text-lg font-semibold text-white -mb-3">Engagement</p>
-        <div className="h-[342px]  rounded-2xl">
+        <div className="h-[342px] rounded-2xl">
           <App />
         </div>
       </div>
